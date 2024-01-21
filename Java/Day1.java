7f5ba85c97e792b2e5f81ed5c7d5df06b77cc33a
Aarushi <aarushiajay4@gmail.com>
2024-01-21 21:23:23 +0530

    We start here

---
+++ Java/Day1.java
@@ -0,0 +1,17 @@
+public class Day1 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    for(int i =1;i<10;i++){
+        System.out.println(i);
+    }
+
+    String str = "aarushi";
+
+    for(char i= 0;i<str.length();i++){
+        System.out.println(str[i]);
+    }
+  }
+}
+
+// In Java, every application begins with a class name, and that class must match the filename.
